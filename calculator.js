5333ac2c45e0770b563e8ccc4f3ac185c9aa3553
Jonathon Day <jonathonday@Jonathons-MacBook-Air.local>
2018-07-29 23:31:07 +0100

    basic operator functions, operate function and html layout

---
+++ calculator.js
@@ -0,0 +1,10 @@
+const add = (num1, num2) => num1 + num2
+const subtract = (num1, num2) => num1 - num2
+const multiply = (num1, num2) => num1 * num2
+const divide = (num1, num2) => num1 / num2
+
+function operate (num1, num2, op) {
+  if(op === '+') {
+    return add(num1, num2);
+  }
+}
