b14fba0714118e711f4228fb02295f04931eb557
Jonathon Day <jonathonday@Jonathons-MacBook-Air.local>
2018-08-04 22:43:29 +0100

    first revisions after comments

--- calculator.js
+++ calculator.js
@@ -1,64 +1,68 @@
-const add = (num1, num2) => num1 + num2
-const subtract = (num1, num2) => num1 - num2
-const multiply = (num1, num2) => num1 * num2
-const divide = (num1, num2) => num1 / num2
-const equalsButton = document.querySelector('button[data-behavior ="equals"]')
-const clearButton = document.querySelector('button[data-behavior ="clear"]')
-let textView = document.querySelector('div[data-behavior ="view"]')
+
+const equalsButton = document.querySelector('button[data-behavior="equals"]')
+const clearButton = document.querySelector('button[data-behavior="clear"]')
+const textView = document.querySelector('div[data-behavior="view"]')
 let calculation = []
 let num1 = ''
 let num2 = ''
 let currentOperator = ''
 
-function equals () {
-  switch (currentOperator) {
-    case '+':
-      add(num1, num2)
-      textView.innerHTML = (add(num1, num2))
-      break
-    case '-':
-      subtract(num1, num2)
-      textView.innerHTML = (subtract(num1, num2))
-      break
-    case 'x':
-      multiply(num1, num2)
-      textView.innerHTML = (multiply(num1, num2))
-      break
-    case '/':
-      divide(num1, num2)
-      textView.innerHTML = (divide(num1, num2))
-      break
-  }
-}
 
-// type numbers into the calculator display.
-document.querySelector('div[data-behavior="btn-container"]').addEventListener('click', function (e) {
-  if (e.target.dataset.input === 'number') {
-    calculation.push(e.target.innerHTML)
-    textView.innerHTML = calculation.join('')
-    console.log(calculation)
-  }
-})
-// type operators into the calculator display
-document.querySelector('div[data-behavior="btn-container"]').addEventListener('click', function (e) {
-  if (e.target.dataset.input === 'operator') {
+
+// type operators into the calculator display.
+let operatorButtons = document.querySelectorAll('button[data-behavior="operator"]')
+let addOperators = function (e) {
+  e.addEventListener('click', function (e) {
     currentOperator = (e.target.innerHTML)
     textView.innerHTML = currentOperator
     let x = calculation.join('')
     num1 = Number(x)
     calculation = []
-    console.log(currentOperator)
-    console.log(num1)
+  }, false)
+}
+for (var i = 0; i < operatorButtons.length; i++) {
+  addOperators(operatorButtons[i])
+}
+
+// type numbers into the calculator display.
+document.querySelector('div[data-behavior="btn-container"]').addEventListener('click', function (e) {
+  if (e.target.dataset.behavior === 'number') {
+    calculation.push(e.target.innerHTML)
+    textView.innerHTML = calculation.join('')
   }
 })
-function calculate () {
+
+function conclude () {
   let x = calculation.join('')
   num2 = Number(x)
   equals()
 }
-
+const add = (num1, num2) => num1 + num2
+const subtract = (num1, num2) => num1 - num2
+const multiply = (num1, num2) => num1 * num2
+const divide = (num1, num2) => num1 / num2
+function equals () {
+  switch (currentOperator) {
+    case '+':
+      add(num1, num2)
+      textView.innerHTML = add(num1, num2)
+      break
+    case '-':
+      subtract(num1, num2)
+      textView.innerHTML = subtract(num1, num2)
+      break
+    case 'x':
+      multiply(num1, num2)
+      textView.innerHTML = multiply(num1, num2)
+      break
+    case '/':
+      divide(num1, num2)
+      textView.innerHTML = divide(num1, num2)
+      break
+  }
+}
 equalsButton.addEventListener('click', () => {
-  calculate()
+  conclude()
 })
 function clear () {
   calculation = []
@@ -67,7 +71,6 @@ function clear () {
   currentOperator = ''
   textView.innerHTML = ''
 }
-
 clearButton.addEventListener('click', () => {
   clear()
 })
